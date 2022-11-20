430f3244edfc709ae0ec47a47975ef185c4c791e
Ali Almutawa <alal5051@colorado.edu>
2022-11-19 20:42:11 -0700

    Added clr namespace

--- Structs/RGB.hpp
+++ Structs/RGB.hpp
@@ -1,38 +1,38 @@
 #pragma once
 
 #include <iostream>
-#include <sstream>
 
-struct RGB{
-    int red;
-    int green;
-    int blue;
+namespace clr{
+    struct RGB{
+        int red;
+        int green;
+        int blue;
 
-    RGB():
-    red(0),
-    green(0),
-    blue(0){}
+        RGB():
+        red(0),
+        green(0),
+        blue(0){}
 
+        RGB(int v):
+        red(v),
+        green(v),
+        blue(v){}
 
-    RGB(int v):
-    red(v),
-    green(v),
-    blue(v){}
+        RGB(int r, int g, int b):
+        red(r), green(g), blue(b){}
 
-    RGB(int r, int g, int b){
-        red = r;
-        green = g;
-        blue = b;
-    }
+        bool operator==(RGB rgb){
+            return red == rgb.red && green == rgb.green && blue == rgb.blue;
+        }
 
-    bool operator==(RGB rgb){
-        return this->red == rgb.red && this->green == rgb.green && this->blue == rgb.blue;
-    }
+        friend std::ostream& operator << (std::ostream &os, const RGB &c){
+            return (os << c.red << " " << c.green << " " << c.blue);
+        }
+    };
 
-    friend std::ostream& operator << (std::ostream &os, const RGB &c){
-        return (os << c.red << " " << c.green << " " << c.blue);
-    }
-};
+    const clr::RGB RED =  RGB(255, 0, 0);
 
+    const clr::RGB GREEN = RGB(0, 255, 0);
 
-
+    const clr::RGB BLUE = RGB(0, 0, 255);
+}
